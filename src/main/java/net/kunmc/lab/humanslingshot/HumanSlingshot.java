541e41d94f841af17e34df0159a60c42d82d21ca
maru-256 <maru043@docomo.ne.jp>
2021-11-23 05:07:00 +0900

    setup

--- src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
+++ src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.humanslingshot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class HumanSlingshot extends JavaPlugin {
     @Override
     public void onEnable() {
     }
