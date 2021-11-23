0cebc822daa41a462325287a1c88e0aa715c892f
maru-256 <maru043@docomo.ne.jp>
2021-11-24 08:49:35 +0900

    add command

--- src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
+++ src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
@@ -1,5 +1,9 @@
 package net.kunmc.lab.humanslingshot;
 
+import dev.kotx.flylib.FlyLib;
+import dev.kotx.flylib.command.Command;
+import net.kunmc.lab.configlib.command.ConfigCommand;
+import net.kunmc.lab.configlib.command.ConfigCommandBuilder;
 import net.minecraft.server.v1_16_R3.DedicatedServerProperties;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
@@ -13,6 +17,15 @@ public final class HumanSlingshot extends JavaPlugin {
     @Override
     public void onEnable() {
         config = new Config(this);
+
+        ConfigCommand configCommand = new ConfigCommandBuilder(config).disableReloadCommand().build();
+        FlyLib.create(this, builder -> {
+            builder.command(new Command("slingshot") {
+                {
+                    children(configCommand);
+                }
+            });
+        });
 
         DedicatedServerProperties properties = ((CraftServer) Bukkit.getServer()).getHandle().getServer().getDedicatedServerProperties();
         try {
