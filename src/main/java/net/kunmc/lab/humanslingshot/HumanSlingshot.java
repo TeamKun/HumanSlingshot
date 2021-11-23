7d7339371aaee17e09dbb49ce2cc87ef9b7945f8
maru-256 <maru043@docomo.ne.jp>
2021-11-24 08:40:01 +0900

    add modify allowFlight logic

--- src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
+++ src/main/java/net/kunmc/lab/humanslingshot/HumanSlingshot.java
@@ -1,6 +1,11 @@
 package net.kunmc.lab.humanslingshot;
 
+import net.minecraft.server.v1_16_R3.DedicatedServerProperties;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
 
 public final class HumanSlingshot extends JavaPlugin {
     private Config config;
@@ -8,6 +13,15 @@ public final class HumanSlingshot extends JavaPlugin {
     @Override
     public void onEnable() {
         config = new Config(this);
+
+        DedicatedServerProperties properties = ((CraftServer) Bukkit.getServer()).getHandle().getServer().getDedicatedServerProperties();
+        try {
+            Field field = properties.getClass().getDeclaredField("allowFlight");
+            field.setAccessible(true);
+            field.set(properties, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
